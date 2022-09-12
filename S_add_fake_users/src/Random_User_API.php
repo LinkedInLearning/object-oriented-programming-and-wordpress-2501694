d7bce318d77fb1cea9537e4e9b43a7de1dd8abb9
Gary Kovar <gary@craftpeak.com>
2022-09-12 09:17:15 -0400

    set ends state on 3_2

---
+++ S_add_fake_users/src/Random_User_API.php
@@ -0,0 +1,59 @@
+<?php
+declare( strict_types=1 );
+
+namespace S_Fake_Users;
+
+class Random_User_API {
+
+	/**
+	 * Reasons to change.
+	 * - add_user method
+	 */
+
+	private string $url;
+	private array  $inc;
+	private int    $results;
+
+	public function __construct(
+		int $results = 5,
+		array $inc = [
+			'name',
+			'location',
+			'email',
+			'picture',
+		],
+		string $url = 'https://randomuser.me/api/'
+	) {
+		$this->results = $results;
+		$this->inc     = $inc;
+		$this->url     = $url;
+	}
+
+	/**
+	 * @return \WP_User[]
+	 */
+	public function get_users(): array {
+		$response = wp_remote_get( $this->url );
+		$body     = wp_remote_retrieve_body( $response );
+		$data     = json_decode( $body );
+		$users    = array_map( [ $this, 'map_user' ], $data->results );
+
+		return $users;
+	}
+
+	private function url() {
+		return $this->url . '?inc=' . implode( ',', $this->inc ) . '&results=' . $this->results;
+	}
+
+	private function map_user( \stdClass $user ): \WP_User {
+		$user_map = new User_Map();
+
+		$user_map->set_email( $user->email )
+			->set_first( $user->name->first )
+			->set_last( $user->name->last )
+			->set_country( $user->location->country );
+
+		return $user_map->as_wp_user();
+	}
+
+}
---
+++ S_add_fake_users/src/User_Map.php
@@ -0,0 +1,72 @@
+<?php
+declare( strict_types=1 );
+
+
+namespace S_Fake_Users;
+
+class User_Map {
+
+	private string $email;
+	private string $first;
+	private string $last;
+	private string $country;
+
+	/**
+	 * @param string $first
+	 *
+	 * @return User_Map
+	 */
+	public function set_first( string $first ): User_Map {
+		$this->first = $first;
+
+		return $this;
+	}
+
+	/**
+	 * @param string $last
+	 *
+	 * @return User_Map
+	 */
+	public function set_last( string $last ): User_Map {
+		$this->last = $last;
+
+		return $this;
+	}
+
+	/**
+	 * @param string $country
+	 *
+	 * @return User_Map
+	 */
+	public function set_country( string $country ): User_Map {
+		$this->country = $country;
+
+		return $this;
+	}
+
+	/**
+	 * @param string $email
+	 *
+	 * @return User_Map
+	 */
+	public function set_email( string $email ): User_Map {
+		$this->email = $email;
+
+		return $this;
+	}
+
+	public function as_wp_user() : \WP_User {
+		$wp_user = new \WP_User();
+
+		$wp_user->user_email = $this->email;
+		$wp_user->user_login = $this->first . ' ' . $this->last;
+		$wp_user->user_pass  = wp_generate_password();
+		$wp_user->first_name = $this->first;
+		$wp_user->last_name  = $this->last;
+
+		$wp_user->locale = $this->country;
+
+		return $wp_user;
+	}
+
+}
