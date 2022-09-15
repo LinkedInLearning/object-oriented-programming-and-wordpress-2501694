1258b5465580f6fc58c68988ff21f25e2a67361b
Gary Kovar <gary@craftpeak.com>
2022-09-15 09:22:57 -0400

    initial commit for 03_06 end state

---
+++ SOLI_add_fake_users/src/User_API.php
@@ -0,0 +1,11 @@
+<?php
+declare( strict_types=1 );
+
+namespace SOLI_Fake_Users;
+
+interface User_API {
+	/**
+	 * @return User_Collection
+	 */
+	public function get_users( int $count = 5 ): User_Collection;
+}
