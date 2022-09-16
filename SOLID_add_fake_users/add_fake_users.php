7838d1faa7d033db00666a3bda4ac06646ac45c6
Gary Kovar <gary@craftpeak.com>
2022-09-16 08:34:41 -0400

    set end state for 03_07

---
+++ SOLID_add_fake_users/add_fake_users.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Plugin Name: SOLID Add Fake Users
+ * Description: Add fake users from the randomuser.me API
+ * Version: 1.0.0
+ * Requires PHP: 7.4
+ * Author: Gary Kovar
+ * License: GPLv2
+ */
+
+require __DIR__ . '/vendor/autoload.php';
+
+if ( defined( 'WP_CLI' ) && WP_CLI ) {
+	$random_user_api = new \SOLID_Fake_Users\Random_User_API();
+
+	WP_CLI::add_command( 'get-fake-users', function ( $args, $assoc_args ) use ( $random_user_api ) {
+		$user_import = new \SOLID_Fake_Users\CLI\User_Import( $random_user_api );
+		$user_import->save( $args, $assoc_args );
+	} );
+}
---
+++ SOLID_add_fake_users/src/Random_User_API.php
@@ -0,0 +1,57 @@
+<?php
+declare( strict_types=1 );
+
+namespace SOLID_Fake_Users;
+
+class Random_User_API implements User_API {
+	private string $url;
+	private array  $inc;
+	private int    $results;
+
+	public function __construct(
+		array $inc = [
+			'name',
+			'location',
+			'email',
+			'picture',
+		],
+		string $url = 'https://randomuser.me/api/'
+	) {
+		$this->inc = $inc;
+		$this->url = $url;
+	}
+
+	/**
+	 * @return User_Collection
+	 */
+	public function get_users( int $count = 5 ): User_Collection {
+		$this->results = $count;
+		$response      = wp_remote_get( $this->build_url() );
+		$body          = wp_remote_retrieve_body( $response );
+		$data          = json_decode( $body );
+		$users         = array_map( [ $this, 'map_user' ], $data->results );
+
+		$user_collection = new User_Collection();
+		foreach ( $users as $user ) {
+			$user_collection->add( $user );
+		}
+
+		return $user_collection;
+	}
+
+	private function build_url() {
+		return $this->url . '?inc=' . implode( ',', $this->inc ) . '&results=' . $this->results;
+	}
+
+	private function map_user( \stdClass $user ): \WP_User {
+		$user_map = new User_Map();
+
+		$user_map->set_email( $user->email )
+				 ->set_first( $user->name->first )
+				 ->set_last( $user->name->last )
+				 ->set_country( $user->location->country );
+
+		return $user_map->as_wp_user();
+	}
+
+}
