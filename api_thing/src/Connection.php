7b07bcf53e3c941bbfb6bfe9ce931af58d65daef
Gary Kovar <gary@craftpeak.com>
2022-11-11 09:36:04 -0500

    set end state for 02_03

--- api_thing/src/Connection.php
+++ api_thing/src/Connection.php
@@ -6,4 +6,15 @@ namespace API_Thing;
 
 class Connection {
 
+	private string $url;
+
+	private string $username;
+
+	private string $password;
+
+	public function __construct( string $url, string $username, string $password ) {
+		$this->url      = $url;
+		$this->username = $username;
+		$this->password = $password;
+	}
 }
--- api_thing/src/Options.php
+++ api_thing/src/Options.php
@@ -5,4 +5,28 @@ namespace API_Thing;
 
 class Options {
 
+	private string $option_name;
+
+	private array $options = [];
+
+	public function __construct( string $option_name ) {
+		$this->option_name = $option_name;
+		$this->options = get_option( $this->option_name );
+	}
+
+	public function save_options() {
+		update_option( $this->option_name, $this->options );
+	}
+
+	public function set_value( string $key, $value ) {
+		$this->options[ $key ] = $value;
+	}
+
+	public function get_value( string $key ) {
+		if ( array_key_exists( $key, $this->options ) ) {
+			return $this->options[ $key ];
+		}
+
+		return '';
+	}
 }
