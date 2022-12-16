995ef6585ca038910a8fcf079900455ee1a7ab75
Gary Kovar <gary@craftpeak.com>
2022-12-16 08:31:26 -0500

    setup start state

--- event_plugin/event_plugin.php
+++ event_plugin/event_plugin.php
@@ -15,23 +15,12 @@ use Event_Plugin\Rest_Request_Filter;
 
 require __DIR__ . '/vendor/autoload.php';
 
-//$event = new Event();
-//
-//remove_action( 'init', [ $event, 'register_taxonomy' ] );
+$event = new Event();
 
-//$rest_request_filter = new Rest_Request_Filter();
+remove_action( 'init', [ $event, 'register_taxonomy' ] );
 
-//add_filter( 'rest_request_before_callbacks', [ $rest_request_filter, 'rest_request_before_callbacks' ] );
-//add_filter( 'rest_request_before_callbacks', function ( \WP_REST_Request $request ) {
-//	$rest_request_filter = new Rest_Request_Filter();
-//
-//	return $rest_request_filter->rest_request_before_callbacks( $request );
-//} );
+$rest_request_filter = new Rest_Request_Filter();
 
-\Event_Plugin\Hooks_For_Objects::add_filter(
-	'rest_request_before_callbacks',
-	'Event_Plugin\Rest_Request_Filter',
-	'rest_request_before_callbacks'
-);
+add_filter( 'rest_request_before_callbacks', [ $rest_request_filter, 'rest_request_before_callbacks' ] );
 
 
